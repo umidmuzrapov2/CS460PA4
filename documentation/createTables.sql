a7adb6792f3fe6a998c41aca925bce98a45e0530
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2023-12-03 20:27:47 -0700

    Update createTables.sql

--- documentation/createTables.sql
+++ documentation/createTables.sql
@@ -1,202 +1,162 @@
-DROP TABLE groupIBMU.Enrollment;
-DROP TABLE groupIBMU.EquipmentLoan;
-DROP TABLE groupIBMU.Purchase;
-DROP TABLE groupIBMU.CoursePackage;
-DROP TABLE groupIBMU.MembershipLevel;
-DROP TABLE groupIBMU.Equipment;
-DROP TABLE groupIBMU.Transaction;
-DROP TABLE groupIBMU.Member;
-DROP TABLE groupIBMU.Schedule;
-DROP TABLE groupIBMU.Course;
-DROP TABLE groupIBMU.Trainer;
-DROP TABLE groupIBMU.Package;
-
-CREATE DOMAIN umidmuzrapov.PossiblePrice AS DECIMAL(6,2)
-	CHECK (VALUE BETWEEN 0 AND 9999.99);
-	
-CREATE DOMAIN umidmuzrapov.Discount AS INTEGER
-	CHECK (VALUE BETWEEN 0 AND 50);
-
-CREATE DOMAIN umidmuzrapov.ParticipantNumber AS INTEGER
-	CHECK (VALUE BETWEEN 0 and 60);
-
-CREATE DOMAIN umidmuzrapov.Day AS INTEGER
-	CHECK (VALUE BETWEEN 0 and 8);
-
-CREATE DOMAIN umidmuzrapov.Hour AS INTEGER
-	CHECK (VALUE BETWEEN -1 AND 25);
-
-CREATE DOMAIN umidmuzrapov.MinuteSecond AS INTEGER
-	CHECK (VALUE BETWEEN -1 AND 61);
-
-CREATE DOMAIN umidmuzrapov.Duration AS INTEGER
-	CHECK (VALUE BETWEEN -1 and 120);
+DROP TABLE umidmuzrapov.Enrollment;
+DROP TABLE umidmuzrapov.EquipmentLoan;
+DROP TABLE umidmuzrapov.Purchase;
+DROP TABLE umidmuzrapov.CoursePackage;
+DROP TABLE umidmuzrapov.MembershipLevel;
+DROP TABLE umidmuzrapov.Equipment;
+DROP TABLE umidmuzrapov.Transaction;
+DROP TABLE umidmuzrapov.Member;
+DROP TABLE umidmuzrapov.Schedule;
+DROP TABLE umidmuzrapov.Course;
+DROP TABLE umidmuzrapov.Trainer;
+DROP TABLE umidmuzrapov.Package;
 
 -- Trainer Table
-CREATE TABLE groupIBMU.Trainer (
+CREATE TABLE umidmuzrapov.Trainer (
     	trainerNumber INTEGER NOT NULL,
     	fname VARCHAR(20) NOT NULL,
     	lname VARCHAR(20) NOT NULL,
     	phoneNumber CHAR(10),
-	PRIMARY KEY (trainerNumber)	
+		PRIMARY KEY (trainerNumber)	
 );
 
 -- Package Table
-CREATE TABLE groupIBMU.Package (
-    	packageName VARCHAR(120) NOT NULL,
-    	cost umidmuzrapov.PossiblePrice NOT NULL,
-	PRIMARY KEY (packageName)
+CREATE TABLE umidmuzrapov.Package (
+    packageName VARCHAR(120) NOT NULL,
+    cost DECIMAL(6,2) NOT NULL,
+    CONSTRAINT PackageCourse CHECK (cost BETWEEN 0 AND 9999.99),
+    PRIMARY KEY (packageName)
 );
 
+
 -- Membership Level Table
-CREATE TABLE groupIBMU.MembershipLevel (
+CREATE TABLE umidmuzrapov.MembershipLevel (
     	levelId INTEGER NOT NULL,
-    	minSpending umidmuzrapov.PossiblePrice NOT NULL,
-    	discount umidmuzrapov.Discount NOT NULL,
-	PRIMARY KEY (levelId)
+    	minSpending DECIMAL(6,2) NOT NULL,
+		CONSTRAINT PossibleCost CHECK (minSpending BETWEEN 0 AND 9999.99),
+    	discount INTEGER NOT NULL,
+		CONSTRAINT Discount CHECK (discount BETWEEN 0 AND 50),
+		PRIMARY KEY (levelId)
 );
 
 -- Member Table
-CREATE TABLE groupIBMU.Member (
-    	memberNumber INTEGER NOT NULL,
-    	fname VARCHAR(20) NOT NULL,
-    	lname VARCHAR(20) NOT NULL,
-    	phoneNumber CHAR(10),
-    	levelId INTEGER,
-	PRIMARY KEY (memberNumber),
-	FOREIGN KEY (levelId) REFERENCES groupIBMU.MembershipLevel
-		ON DELETE SET NULL
-		ON UPDATE CASCADE
+CREATE TABLE umidmuzrapov.Member (
+    memberNumber INTEGER NOT NULL,
+    fname VARCHAR(20) NOT NULL,
+    lname VARCHAR(20) NOT NULL,
+    phoneNumber CHAR(10),
+    levelId INTEGER,
+    PRIMARY KEY (memberNumber),
+    FOREIGN KEY (levelId) REFERENCES umidmuzrapov.MembershipLevel ON DELETE SET NULL
 );
 
+
 -- Course Table
-CREATE TABLE groupIBMU.Course (
-	className VARCHAR(120) NOT NULL,
-	maxParticipant umidmuzrapov.ParticipantNumber,
-    	currentParticipant INTEGER 
+CREATE TABLE umidmuzrapov.Course (
+		className VARCHAR(120) NOT NULL,
+		maxParticipant INTEGER,
+		CONSTRAINT ParticipantMax CHECK (maxParticipant BETWEEN 0 and 60),
+    	currentParticipant INTEGER, 
 			CONSTRAINT CurrentParticipant
-			CHECK (VALUE BETWEEN 0 AND maxParticipant),
+			CHECK (currentParticipant BETWEEN 0 AND maxParticipant),
     	startDate DATE NOT NULL,
     	endDate DATE NOT NULL,
     	trainerNumber INTEGER NOT NULL,
     	PRIMARY KEY (className, startDate),
-	FOREIGN KEY (trainerNumber) REFERENCES groupIBMU.Trainer
-					ON DELETE NO ACTION
-					ON UPDATE CASCADE
- 
+		FOREIGN KEY (trainerNumber) REFERENCES umidmuzrapov.Trainer
 );
 
 
 -- Schedule Table
-CREATE TABLE groupIBMU.Schedule (
+CREATE TABLE umidmuzrapov.Schedule (
 	className VARCHAR(120) NOT NULL,
     	startDate DATE NOT NULL,
-    	day VARCHAR(10) NOT NULL,
-    	hour umidmuzrapov.Hour NOT NULL,
-    	minute umidmuzrapov.MinuteSecond NOT NULL,
-    	duration umidmuzrapov.Duration NOT NULL,
+    	day INTEGER NOT NULL,
+		CONSTRAINT Day CHECK (day BETWEEN 0 and 8) ,
+    	hour INTEGER NOT NULL,
+		CONSTRAINT Hour CHECK (hour BETWEEN -1 AND 25) ,
+    	minute INTEGER NOT NULL,
+		CONSTRAINT Minute CHECK (minute BETWEEN -1 AND 61) ,
+    	duration INTEGER NOT NULL,
+		CONSTRAINT Duration CHECK (duration BETWEEN -1 and 120) ,
     	PRIMARY KEY (className, startDate, day),
-	FOREIGN KEY (className) REFERENCES groupIBMU.Course
-					ON UPDATE CASCADE,
-
-	FOREIGN KEY (startDate) REFERENCES groupIBMU.Course
-					ON UPDATE CASCADE
-
+		FOREIGN KEY (className, startDate) REFERENCES umidmuzrapov.Course
 );
 
 -- Equipment Table
-CREATE TABLE groupIBMU.Equipment (
-	itemNumber INTEGER NOT NULL,
-    	description VARCHAR(120),
+CREATE TABLE umidmuzrapov.Equipment (
+		itemNumber INTEGER NOT NULL,
+    	descriptionComment VARCHAR(120),
     	quantity INTEGER NOT NULL,
-	PRIMARY KEY (itemNumber)
+		PRIMARY KEY (itemNumber)
 );
 
 -- CoursePackage Table
-CREATE TABLE groupIBMU.CoursePackage (
+CREATE TABLE umidmuzrapov.CoursePackage (
     	packageName VARCHAR(120) NOT NULL,
-   	className VARCHAR(120) NOT NULL,
+   		className VARCHAR(120) NOT NULL,
     	startDate DATE NOT NULL,
     	PRIMARY KEY (packageName, className, startDate),
-	FOREIGN KEY (packageName) REFERENCES groupIBMU.Package
-	    				ON UDPATE CASCADE
-					ON DELETE NO ACTION
-	FOREIGN KEY (className) REFERENCES groupIBMU.Course
-					ON UPDATE CASCADE
-					ON DELETE NO ACTION
-	FOREIGN KEY (startDate) REFERENCES groupIBMU.Course
-					ON UPDATE CASCADE
-					ON DELETE NO ACTION
-	
+		FOREIGN KEY (packageName) REFERENCES umidmuzrapov.Package,
+		FOREIGN KEY (className, startDate) REFERENCES umidmuzrapov.Course
 );
 
 
 -- Transaction Table
-CREATE TABLE groupIBMU.Transaction (
+CREATE TABLE umidmuzrapov.Transaction (
     	transactionNumber INTEGER NOT NULL,
-    	total PossiblePrice NOT NULL,
+    	total DECIMAL(6,2) NOT NULL,
+		CONSTRAINT Cost CHECK (total BETWEEN 0 AND 9999.99),
     	transactionDate DATE NOT NULL,
     	type VARCHAR(50),
     	memberNumber INTEGER NOT NULL,
-	PRIMARY KEY (transactionNumber),
-	FOREIGN KEY (memberNumber) REFERENCES groupIBMU.Course
-					ON UDPATE CASCADE
-					ON DELETE ON ACTION
+		PRIMARY KEY (transactionNumber),
+		FOREIGN KEY (memberNumber) REFERENCES umidmuzrapov.Member
 );
 
 -- Purchase Table
-CREATE TABLE groupIBMU.Purchase (
-    	packageName VARCHAR(255) NOT NULL,
-    	transactionNumber INTEGER NOT NULL,
-    	PRIMARY KEY (packageName, transactionNumber),
-	FOREIGN KEY (packageName) REFERENCES groupIBMU.Package
-	    				ON UDPATE CASCADE
-					ON DELETE NO ACTIOM,
-	FOREIGN KEY (transactionNumber) REFERENCES groupIBMU.Transaction
-					ON UPDATE CASCADE
-					ON DELETE NO ACTION
+CREATE TABLE umidmuzrapov.Purchase (
+    packageName VARCHAR(120) NOT NULL,
+    transactionNumber INTEGER NOT NULL,
+    PRIMARY KEY (packageName, transactionNumber),
+    FOREIGN KEY (packageName) REFERENCES umidmuzrapov.Package(packageName),
+    FOREIGN KEY (transactionNumber) REFERENCES umidmuzrapov.Transaction(transactionNumber)
 );
 
 -- EquipmentLoan Table
-CREATE TABLE groupIBMU.EquipmentLoan (
+CREATE TABLE umidmuzrapov.EquipmentLoan (
     	loanNumber INTEGER NOT NULL,
     	itemNumber INTEGER NOT NULL,
     	memberItem INTEGER NOT NULL,
-    	quantity INTEGER CONTRAINT CHECK (VALUE >= 0),
+    	quantity INTEGER,
+		CONSTRAINT Quantity CHECK (quantity > 0 OR quantity=0),
     	borrowedDate DATE,
     	returnedDate DATE,
-	PRIMARY KEY (loanNumber),
-	FOREIGN KEY (item) REFERENCES groupIBMU.Equipment
-				ON UPDATE CASCADE
-				ON DELETE NO ACTION
-	FOREIGN KEY (memberItem) REFERENCES groupIBMU.Member
-				ON UPDATE CASCADE
-				ON DELETE NO ACTION
+		PRIMARY KEY (loanNumber),
+		FOREIGN KEY (itemNumber) REFERENCES umidmuzrapov.Equipment,
+		FOREIGN KEY (memberItem) REFERENCES umidmuzrapov.Member
 );
 
 -- Enrollment Table
-CREATE TABLE groupIBMU.Enrollment (
+CREATE TABLE umidmuzrapov.Enrollment (
     	memberNumber INTEGER NOT NULL,
     	courseName VARCHAR(120) NOT NULL,
     	startDate DATE NOT NULL,
      	PRIMARY KEY (memberNumber, courseName, startDate),
-     	FOREIGN KEY (memberNumber) REFERENCES groupIBMU.Member
-				ON UPDATE CASCADE
-				ON DELETE NO ACTION,
-	FOREIGN KEY (memberNumebr) REFERENCES groupIBMU.Course
-				ON UPDATE CASCADE
+     	FOREIGN KEY (memberNumber) REFERENCES umidmuzrapov.Member,
+		FOREIGN KEY (courseName, startDate) REFERENCES umidmuzrapov.Course
 );
 
 -- Grant permissions to group members and TAs
-GRANT ALL PRIVILEGES ON groupIBMU.Package TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Trainer TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Course TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Schedule TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Member TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Transaction TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Equipment TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.MembershipLevel TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.CoursePackage TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Purchase TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.EquipmentLoan TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
-GRANT ALL PRIVILEGES ON groupIBMU.Enrollment TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Package TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Trainer TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Course TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Schedule TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Member TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Transaction TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Equipment TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.MembershipLevel TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.CoursePackage TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Purchase TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.EquipmentLoan TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
+GRANT ALL PRIVILEGES ON umidmuzrapov.Enrollment TO marhoon, umidmuzrapov, yahyaalmalallah, danialb, qzydustin;
